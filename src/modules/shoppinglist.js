25385e77a4e8966e2b72cb98ee6a6b5d9c54d36e
as-bowley <as.bowley@gmail.com>
2022-06-14 07:22:09 +0200

    init commit

---
+++ src/modules/shoppinglist.js
@@ -0,0 +1,26 @@
+class ShoppingList {
+  constructor(item) {
+    this.item = item;
+  }
+
+  add() {
+    //add new item to list
+  }
+
+  edit() {
+    //edit one of the current list items
+  }
+
+  remove() {
+    //remove item from list
+  } 
+
+  get() {
+    //get information from list item for display
+  }
+
+  markAsComplete() {
+    //mark item as complete
+  }
+
+}
---
+++ src/modules/todolistlong.js
@@ -0,0 +1,29 @@
+class TodoListLong {
+  constructor(title, desc, prio, date) {
+    this.title = title;
+    this.desc = desc;
+    this.prio = prio;
+    this.date = date;
+  }
+
+  add() {
+    //add new item to list
+  }
+
+  edit() {
+    //edit one of the current list items
+  }
+
+  remove() {
+    //remove item from list
+  } 
+
+  get() {
+    //get information from list item for display
+  }
+
+  markAsComplete() {
+    //mark item as complete
+  }
+
+}
